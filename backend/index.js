919d0cff79099409e9d398d1306229c8a04aa615
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-13 15:34:04 +0200

    connected to DB

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,38 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import './env_config.js'
+
+const DB_CONNECTION = process.env.DB_CONNECTION
+const PORT_SERVER = process.env.PORT_SERVER
+
+const app = express()
+
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mongoose.connect(DB_CONNECTION)
+.then(() => {
+    app.listen(PORT_SERVER, () => console.log("Server running on PORT"+ " " + PORT_SERVER + " 👍"))
+    console.log("Connected to DB 👍")
+})
+.catch((err) => console.log(err + "ERROR - not able to connect to DB 👎"))
