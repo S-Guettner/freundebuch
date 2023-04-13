f115413a77d390caf5754653a75b5a54300df312
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-13 15:55:01 +0200

    feat:added new contact route

--- backend/index.js
+++ backend/index.js
@@ -28,8 +28,8 @@ app.post('/api/v1/new-contact' , async (req,res) => {
         const data = await contactModel.create(req.body)
         res.status(200).json(data)
     } catch (err) {
-        console.log(error.message)
-        res.status(500).json({message: error.message})
+        console.log(err.message)
+        res.status(500).json({message: err.message})
     }
 })
 
